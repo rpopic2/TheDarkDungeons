e642231ae4e1d98f49e07918bbcd009e17d3e39f
rpopic <rpopic2@outlook.com>
2021-12-28 15:03:48 +0000

    test

---
+++ Charactor.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+public enum ClassName
+{
+    Warrrior, Assassin, Mage
+}
+public class Charactor
+{
+
+    public string charName = "Michael";
+    public ClassName charClass;
+    public int hp = 10;
+    int lv = 0;
+    int exp = 0;
+    const float lvCurve = 1.25f;
+    const int lvMultiplier = 10;
+    public int str = 4;
+    public int dex = 4;
+    public int wis = 4;
+
+    public int MaxExp
+    {
+        get { return (int)MathF.Floor(lv * lvMultiplier * lvCurve); }
+    }
+
+    public void PrintStats()
+    {
+        string result = $"Name : {charName} | Class : {charClass.ToString()} | Level : {lv} | Experience : {exp}\nStrength : | {str} Dexterity : | {dex} Wisdom : {wis}";
+        Console.WriteLine(result);
+    }
+
+    public void GainExp(int exp)
+    {
+        this.exp += exp;
+        if (exp >= MaxExp)
+        {
+            exp -= MaxExp;
+            LvUp();
+        }
+    }
+
+    void LvUp()
+    {
+        exp = 0;
+        lv++;
+        ClassSwitch(()=>{str+=2;}, ()=>{dex+=2;}, ()=>{wis+=2;});
+        Console.WriteLine("Level up!");
+    }
+    public void ClassSwitch(Action warrior, Action assassin, Action mage)
+    {
+        switch (charClass)
+        {
+            case ClassName.Warrrior:
+                warrior();
+                break;
+            case ClassName.Assassin:
+                assassin();
+                break;
+            case ClassName.Mage:
+                mage();
+                break;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿pr("The Dark Dungeon ver 0.1\nPress any key to start...");
+Console.ReadKey();
+
+Console.Clear();
+//Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+Charactor charactor = new Charactor();
+
+pr("Choose charactor`s name...");
+charactor.charName = Console.ReadLine() ?? "Michael";
+
+pr("Choose your class...");
+sel(new string[] = ("Warrior", "Assassin", "Mage"));
+char temp = Console.ReadKey().KeyChar;
+
+charactor.GainExp(1);
+charactor.PrintStats();
+
+void pr(object x)
+{
+    Console.WriteLine(x);
+}
+
+void sel(string[] options)
+{
+    string printResult = "/";
+    foreach (string item in options)
+    {
+        string tempItem = item;
+        item.Insert(0, "(");
+        item.Insert(2, ")");
+        printResult += $" {tempItem} /";
+    }
+    pr(printResult);
+}
