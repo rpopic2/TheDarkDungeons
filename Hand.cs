710a57b0c1597b5fb229373f6a3598eaebe13d39
rpopic <rpopic2@outlook.com>
2022-01-08 10:59:31 +0000

    f rest -> q

--- Hand.cs
+++ Hand.cs
@@ -32,7 +32,11 @@ public struct Hand
         IO.pr("You've found a card." + card);
         IO.pr(this);
         IO.sel(Player.hand.Cur, out int index, out char key, out bool cancel);
-        if(cancel) return;
+        if (cancel)
+        {
+            IO.del();
+            return;
+        }
         _Pickup(index, card, silent);
         IO.del(2);
     }
