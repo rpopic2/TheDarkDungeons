0baf475593a866f24e269b1d10bcac2b246b6a95
rpopic <rpopic2@outlook.com>
2022-01-16 05:05:42 +0000

    f

--- Rules.cs
+++ Rules.cs
@@ -2,6 +2,6 @@ public static class Rules
 {
     public const string version = "0.1";
     public const float vulMulp = 1.3f;
-    public const bool MapDebug = false;
+    public static readonly bool MapDebug = false;
     public static readonly bool SkipIntro = true;
 }
