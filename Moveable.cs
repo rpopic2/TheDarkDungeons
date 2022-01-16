1dc54e3853ac81a352ab5f826a71d671dc23d10c
rpopic <rpopic2@outlook.com>
2022-01-16 01:40:42 +0000

    position accessablity

--- Moveable.cs
+++ Moveable.cs
@@ -1,6 +1,6 @@
 public class Moveable : Entity
 {
-    public Position position;
+    public Position position {get; protected set;}
 
     public Moveable(string name, ClassName className, int cap, int maxHp, int lv, int sol, int lun, int con) : base(name, className, cap, maxHp, lv, sol, lun, con)
     {
