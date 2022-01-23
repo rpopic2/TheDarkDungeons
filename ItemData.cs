a55c91658914b2ab333c7323896428c58ba5d8e2
chaeripicker <rpopic2@outlook.com>
2022-01-23 04:57:57 +0000

    record

--- ItemData.cs
+++ ItemData.cs
@@ -1,16 +1,8 @@
-public readonly struct ItemData
+public readonly record struct ItemData (string abv, int amount)
 {
     public static readonly ItemData HpPot = new("HPPOT", 3);
-    public static readonly ItemData AmuletOfLa = new("AMULA", 30);
+    public static readonly ItemData AmuletOfLa = new("AMULA", 30 );
 
-    public readonly string abv;
-    public readonly int amount;
-
-    public ItemData(string abv, int amount)
-    {
-        this.abv = abv;
-        this.amount = amount;
-    }
     public override string ToString()
     {
         return $"[{abv}]";
