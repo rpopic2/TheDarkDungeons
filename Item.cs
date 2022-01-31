fb954c6b19ee9f90688bbb8cd9175a9f765ada9f
chaeripicker <rpopic2@outlook.com>
2022-01-31 13:53:12 +0000

    tweak

--- Item.cs
+++ Item.cs
@@ -1,11 +1,4 @@
-public readonly record struct Item(string abv, ItemType itemType, Action<Fightable>? onUse, Action<Fightable>? onExile = null) : IItem
-{
-    public override string ToString()
-    {
-        if (abv is null) return "[EMPTY]";
-        return $"[{abv}]";
-    }
-}
+public readonly record struct Item(string abv, ItemType itemType, Action<Fightable>? onUse, Action<Fightable>? onExile = null) : IItem;
 public class ItemEntity : IItem
 {
     public ItemEntity(Item data, Fightable owner)
@@ -29,45 +22,6 @@ public class ItemEntity : IItem
         return $"[{abv}{stack}]";
     }
 }
-// public readonly record struct Equip : IItem
-// {
-//     private readonly (int stat, int amount)[] mods;
-//     public string Abv { get; init; }
-//     public ItemType itemType { get; init; } = ItemType.Equip;
-//     public Action<Fightable>? onUse { get; init; } = null;
-//     public readonly Action<Fightable> onPickup;
-//     public readonly Action<Fightable> onExile;
-
-//     public Equip(string abv, params (int stat, int amount)[] mods)
-//     {
-//         Abv = abv;
-//         this.mods = mods;
-//         Abv = abv;
-//         this.itemType = itemType;
-//         this.onUse = onUse;
-//         onPickup = f =>
-//    {
-//        for (int i = 0; i < mods.Length; i++)
-//        {
-//            mods[i].stat += mods[i].amount;
-//        }
-//    };
-//         onExile = f =>
-//    {
-//        for (int i = 0; i < mods.Length; i++)
-//        {
-//            mods[i].stat -= mods[i].amount;
-//        }
-//    };
-//     }
-
-//     public override string ToString()
-//     {
-//         if (Abv is null) return "[EMPTY]";
-//         return $"<{Abv}>";
-//     }
-// }
-
 public interface IItem
 {
     string abv { get; init; }
