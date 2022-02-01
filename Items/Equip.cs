ea00b3b16bac1af0bfdccc9d7422552e76af89b6
chaeripicker <rpopic2@outlook.com>
2022-02-01 08:23:46 +0000

    tweaks

--- Items/Equip.cs
+++ Items/Equip.cs
@@ -1,29 +1,26 @@
 public record Equip : ItemEntity
 {
     public delegate ref int RefInt();
-    private (Func<Inventoriable, Equip.RefInt> stat, int amount)[] mods;
-    public Equip(Inventoriable owner, Stat ownerStat, EquipData2 data) : base(data.abv, ItemType.Equip, ownerStat)
+    private (Func<Inventoriable, RefInt> stat, int amount)[] mods;
+    public new Action onUse { get; init; }
+    public new Action onExile { get; init; }
+    public Equip(Inventoriable owner, Stat ownerStat, EquipData data) : base(data.abv, ItemType.Equip, ownerStat)
     {
         this.mods = data.mods;
-        onUse = (f) =>
+        Action<bool> Invoker = (b) =>
         {
             for (int i = 0; i < mods.Length; i++)
             {
                 RefInt o = mods[i].stat(owner);
                 ref int stat = ref o();
-                stat += mods[i].amount;
-            }
-
-        };
-        onExile = (f) =>
-        {
-            for (int i = 0; i < mods.Length; i++)
-            {
-                RefInt o = mods[i].stat(owner);
-                ref int stat = ref o();
-                stat -= mods[i].amount;
+                if (b) stat += mods[i].amount;
+                else stat -= mods[i].amount;
             }
         };
+        onUse = () => Invoker(true);
+        onExile = () => Invoker(false);
     }
+
+
     public override string ToString() => base.ToString();
 }
