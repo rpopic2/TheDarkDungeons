b7093e642318a25f621ceabbce030d28ca341862
chaeripicker <rpopic2@outlook.com>
2022-02-01 07:49:45 +0000

    tweak

--- Items/Equip.cs
+++ Items/Equip.cs
@@ -1,19 +1,9 @@
-public readonly record struct EquipData
-{
-    public readonly string abv;
-    public delegate ref int Del();
-    public readonly (Del del, int amount)[] mods;
-    public EquipData(string abv, params (Del del, int amount)[] mods)
-    {
-        this.abv = abv;
-        this.mods = mods;
-    }
-}
+public readonly record struct EquipData(string abv, params (Equip.RefInt del, int amount)[] mods);
 
 public record Equip : ItemEntity
 {
-    //public static readonly ItemData data = new("LUNRIN", ItemType.Equip, null);
-    private (EquipData.Del del, int amount)[] mods;
+    public delegate ref int RefInt();
+    private (RefInt del, int amount)[] mods;
     public Equip(Stat ownerStat, EquipData data) : base(data.abv, ItemType.Equip, ownerStat)
     {
         this.mods = data.mods;
