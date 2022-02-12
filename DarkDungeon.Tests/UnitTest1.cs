7831ae002fea99b173d5c2ac64712b88831a8e84
chaeripicker <rpopic2@outlook.com>
2022-02-12 10:00:35 +0000

    testing things out

--- DarkDungeon.Tests/UnitTest1.cs
+++ DarkDungeon.Tests/UnitTest1.cs
@@ -11,4 +11,17 @@ public class UnitTest1
         point -= 1;
         Equals(4 == point.Cur);
     }
+    [Fact]
+    public void Test2()
+    {
+        GamePoint point = new(0, GamePointOption.Stacking);
+        point += 1;
+        Equals(1 == point.Cur);
+    }
+    [Fact]
+    public void Test3()
+    {
+        Program prg = new();
+        Assert.True(prg.GetType() == typeof(Program));
+    }
 }
