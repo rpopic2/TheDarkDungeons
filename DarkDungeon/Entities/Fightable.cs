8bdf48ae47a15cf1a90e537ef3061ed6f6eb433f
cherry <leblanc3385@gmail.com>
2022-04-02 09:28:43 +0900

    refactord unness codes

--- DarkDungeon/Entities/Fightable.cs
+++ DarkDungeon/Entities/Fightable.cs
@@ -80,7 +80,7 @@ public class Fightable : Moveable
         if (!(Target is Fightable fight)) return;
         if (stance.stance == Stance.Attack)
         {
-            string tempString = $"{Name}은 주먹으로 상대를 힘껏 떄렸다. ({stance.amount})";
+            string tempString = $"{Name}은 주먹으로 상대를 힘껏 때렸다. ({stance.amount})";
             TryUseStar();
             IO.pr(tempString);
             fight.TryDodge(stance.amount);
@@ -91,7 +91,7 @@ public class Fightable : Moveable
     {
         if (stance.stance == Stance.Defence)
         {
-            string tempStr = $"{Name}는 굴러서 적의 공격을 피 {stance.amount} damage.";
+            string tempStr = $"{Name}는 굴러서 적의 공격을 피했다. {stance.amount} damage.";
             TryUseStar();
             if (damage <= 0)
             {
@@ -138,7 +138,7 @@ public class Fightable : Moveable
     protected void OnHeal(object? sender, PointArgs e) => IO.pr($"{Name} restored {e.Amount} hp. {Hp}", true);
     protected void OnDamaged(object? sender, PointArgs e)
     {
-        if (e.Amount > 0) IO.pr($"{Name} takes {e.Amount} damage. {Hp}", true);
+        if (e.Amount > 0) IO.pr($"{Name}은 {e.Amount}의 피해를 입었다. {Hp}", true);
     }
 
     public override string ToString() =>
@@ -147,16 +147,6 @@ public class Fightable : Moveable
     {
         if (IsAlive) return base.ToChar();
         else return 'x';
-    }
-    public string GetHandPrivate()
-    {
-        string result = $"Hand : ";
-        foreach (Card? item in Hand.Content)
-        {
-            if (item is not Card card) result += Item.Empty;
-            else result += card.ToStringPrivate();
-        }
-        return result;
     }
     public void UpdateTarget()
     {
