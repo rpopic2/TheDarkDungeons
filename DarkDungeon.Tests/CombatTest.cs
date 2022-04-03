5d1b72b9f6dcdc39f3a2abfcd9d99d4fa50990e3
cherry <leblanc3385@gmail.com>
2022-04-03 11:41:03 +0900

    added combat test

--- DarkDungeon.Tests/CombatTest.cs
+++ DarkDungeon.Tests/CombatTest.cs
@@ -27,4 +27,38 @@ public class CombatTest
         player.Move(4);
         Assert.Equal(new(0, 0), player.Pos);
     }
+    [Fact]
+    public void Combat()
+    {
+        Player._instance = new("Tester", ClassName.Warrior);
+        Player player = Player.instance;
+        Map map = new(5, false);
+        map._Spawn(MonsterDb.lunatic, new(1, 1, Facing.Back));
+        Monster mob = ((Monster)map.Moveables[1]!);
+
+        player.UpdateTarget();
+        Assert.Equal(mob, player.Target);
+
+        player.tokens.Add(TokenType.Offence);
+        try
+        {
+            player.SelectSkillAndUse(Item.bardHand, 0);
+        }
+        catch (System.InvalidOperationException)
+        {
+
+        }
+        Assert.Equal(Stance.Offence, player.CurStance.stance);
+        Assert.Equal(0, player.tokens.Count);
+
+        try
+        {
+            player.TryAttack();
+        }
+        catch (System.InvalidOperationException)
+        {
+
+        }
+        Assert.NotEqual(mob.Hp.Max, mob.Hp.Cur);
+    }
 }
