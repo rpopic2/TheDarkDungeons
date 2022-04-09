e9a6d2ab9b907b00dd9c8937838e82b4c4b28461
cherry <leblanc3385@gmail.com>
2022-04-09 22:23:07 +0900

    yes

--- DarkDungeon/Entities/Monsters/MonsterDb.cs
+++ DarkDungeon/Entities/Monsters/MonsterDb.cs
@@ -8,11 +8,11 @@ public partial class Monster
     private static DropList batDropList = new(
         (Item.bat, 10));
     private static StatMul lunaticMul = new(sol: 1, lun: 1, con: 3, hp: 3, cap: 4, killExp: 4);
-    public static MonsterData lunatic = new("Lunatic", '>', '<', lunaticMul, (m) => m.LunaticBehav(), Item.holySword, new int[] { 2, 0, 2, 0 }, lunDropList);
+    public static MonsterData lunatic = new("광신도", '>', '<', lunaticMul, (m) => m.LunaticBehav(), Item.holySword, new int[] { 2, 0, 2, 0 }, lunDropList);
     private static StatMul snakeMul = new(sol: 2, lun: 1, con: 2, hp: 2, cap: 2, killExp: 5);
-    public static MonsterData snake = new("Snake", 'S', '2', snakeMul, (m) => m.SnakeBehav(), Item.bareHand, new int[] { 2, 0, 0 }, snakeDropList);
+    public static MonsterData snake = new("뱀", 'S', '2', snakeMul, (m) => m.SnakeBehav(), Item.bareHand, new int[] { 2, 0, 0 }, snakeDropList);
     public static StatMul batMul = new(sol: 1, lun: 3, con: 2, hp: 2, cap: 3, killExp: 3);
-    public static MonsterData bat = new("Bat", 'b', 'd', batMul, (m) => m.BatBehav(), Item.bareHand, new int[] { 1, 1, 0 }, batDropList);
+    public static MonsterData bat = new("박쥐", 'b', 'd', batMul, (m) => m.BatBehav(), Item.bareHand, new int[] { 1, 1, 0 }, batDropList);
     public static List<MonsterData> data = new() { lunatic, bat };
     public static int Count => data.Count;
     private void BasicMovement()
@@ -29,7 +29,7 @@ public partial class Monster
             if (Target is null) BasicMovement();
             else
             {
-                if (tokens.Count < 3 && player.Stance.Last == global::StanceName.Charge && tokens.Contains(TokenType.Offence)) SelectSkill(Item.bat, 0);
+                if (tokens.Count < player.tokens.Count && tokens.Contains(TokenType.Offence)) SelectSkill(Item.bat, 0);
                 else if (tokens.Contains(TokenType.Defence)) SelectSkill(Item.bat, 1);
                 else if (tokens.Contains(TokenType.Offence)) SelectSkill(Item.bat, 0);
             }
