91c953662da3e65e522df8d347c3e9e2e4cb8d4c
cherry <leblanc3385@gmail.com>
2022-04-10 21:49:45 +0900

    basic stat nerfed to 2

--- DarkDungeon/Entities/Player.cs
+++ DarkDungeon/Entities/Player.cs
@@ -2,9 +2,8 @@ namespace Entities;
 
 public class Player : Inventoriable
 {
-    public const int skillMax = 2;
     public const int BASICCAP = 3;
-    public const int BASICSTAT = 3;
+    public const int BASICSTAT = 2;
     public static Player? _instance;
     public static Player instance { get => _instance ?? throw new Exception("Player was not initialised"); }
     public ISteppable? underFoot => Map.Current.steppables[Pos.x];
