8e6b8dfdebf6f5b11733abb94d4695a1745bc67e
cherry <leblanc3385@gmail.com>
2022-04-13 23:15:44 +0900

    f typo bug

--- DarkDungeon/Tokens.cs
+++ DarkDungeon/Tokens.cs
@@ -5,20 +5,20 @@ public struct Tokens
     public static readonly char[] TokenSymbols = { '(', '[', '<' };
     public static readonly string[] TokenPromptNames = { "(q) (공격)", "(w) [방어]", "(e) <충전>" };
 
-    private List<TokenType> _array;
+    private List<TokenType> _content;
 
-    public ReadOnlyCollection<TokenType> Content => _array.AsReadOnly();
+    public ReadOnlyCollection<TokenType> Content => _content.AsReadOnly();
 
     public Tokens(int cap)
     {
-        _array = new(cap);
+        _content = new(cap);
     }
 
     public override string ToString()
     {
         string result = "토큰 :";
         if (Count <= 0) return result + " EMPTY";
-        foreach (byte? item in _array)
+        foreach (byte? item in _content)
         {
             if (item is byte value) result += " " + TokenSymbols[value];
         }
@@ -30,39 +30,39 @@ public struct Tokens
     }
     public void Add(TokenType item)
     {
-        if (Count >= _array.Capacity) throw new IndexOutOfRangeException("Your token hand is full. Cannot add more.");
-        _array.Add(item);
+        if (Count >= _content.Capacity) throw new IndexOutOfRangeException("Your token hand is full. Cannot add more.");
+        _content.Add(item);
     }
     public void Remove(TokenType item)
     {
-        Remove(item);
+        _content.Remove(item);
     }
     public void RemoveAt(int index)
     {
-        _array.RemoveAt(index);
+        _content.RemoveAt(index);
     }
     public int IndexOf(TokenType tokenType)
     {
-        return _array.IndexOf(tokenType);
+        return _content.IndexOf(tokenType);
     }
     public bool Contains(TokenType tokenType)
     {
-        return _array.Contains(tokenType);
+        return _content.Contains(tokenType);
     }
     public TokenType? this[int index]
     {
         get
         {
-            if (index >= _array.Count) return null;
-            return _array[index];
+            if (index >= _content.Count) return null;
+            return _content[index];
         }
     }
-    public int Count => _array.Count;
-    public bool IsFull => Count >= _array.Capacity;
+    public int Count => _content.Count;
+    public bool IsFull => Count >= _content.Capacity;
 
     public TokenType? TryUse(TokenType token)
     {
-        if (_array.IndexOf(token) != -1)
+        if (_content.IndexOf(token) != -1)
         {
             Remove(token);
             return token;
