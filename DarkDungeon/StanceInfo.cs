e902cd1b6e272cc1930d7e06bc2575b22dcd53e9
Sakura <leblanc3385@gmail.com>
2022-04-15 18:48:28 +0900

    removed unness StancInfo.Last

--- DarkDungeon/StanceInfo.cs
+++ DarkDungeon/StanceInfo.cs
@@ -1,14 +1,12 @@
 public class StanceInfo
 {
     private StanceName stance;
-    private StanceName last;
     private int amount;
     private int amount2;
 
     public StanceName Stance { get => stance; }
     public int Amount { get => amount; }
     public int Amount2 { get => amount2; }
-    public StanceName Last { get => last;}
 
     public StanceInfo(StanceName stance, int amount, int amount2 = default)
     {
@@ -16,7 +14,6 @@ public class StanceInfo
     }
     public void Set(StanceName stance, int amount, int amount2 = default)
     {
-        last = stance;
         this.stance = stance;
         this.amount = amount;
         this.amount2 = amount2;
