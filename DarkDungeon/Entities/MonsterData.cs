476b93cc3e6b4102ddb6d71713217e0cc5ed82f8
gcloud-main <noreply@gmail.com>
2022-04-17 08:55:02 +0000

    spawn debug mode off

--- DarkDungeon/Entities/MonsterData.cs
+++ DarkDungeon/Entities/MonsterData.cs
@@ -15,7 +15,7 @@ public partial class Monster
     private static StatMul snakeMul = new(sol: 1, lun: 3, con: 1, cap: 3, killExp: 3, Sight: 3);
     public static MonsterData snake = new(name: "뱀", 'S', '2', snakeMul, (m) => m.SnakeBehav(), new Item[] { Fightable.snakeItem }, startToken: new int[] { 2, 0, 0 });
 
-    public static List<MonsterData> data = new() { /*lunatic, bat,*/ snake };
+    public static List<MonsterData> data = new() { lunatic, bat, snake };
     private Fightable? _followTarget;
     public static int Count => data.Count;
     private void BasicMovement()
