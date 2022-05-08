6e6efd6147af2f08091b0d41491057c8c4bc019f
chaeri <noreply@google.com>
2022-05-08 16:43:09 +0900

    fightable is now abstract class

--- DarkDungeon/Fightable.cs
+++ DarkDungeon/Fightable.cs
@@ -1,5 +1,5 @@
 namespace Entities;
-public partial class Fightable
+public abstract partial class Fightable
 {
     public readonly string Name;
     public int Level { get; protected set; }
@@ -35,7 +35,7 @@ public partial class Fightable
     {
         Energy += amount;
     }
-    public virtual void SelectAction() { }
+    public abstract void SelectAction();
     protected void SelectBehaviour(Item item, int index)
     {
         if (Status.CurrentBehav != null) throw new Exception("스탠스가 None이 아닌데 새 동작을 선택했습니다. 한 턴에 두 동작을 할 수 없습니다.");
