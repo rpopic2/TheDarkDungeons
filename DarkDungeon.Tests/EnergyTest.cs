4452c53559bce08dcbd918355ed062c4376fc408
taewhan <noreply@google.com>
2022-05-21 20:28:46 +0900

    added energyconsumetest

--- DarkDungeon.Tests/EnergyTest.cs
+++ DarkDungeon.Tests/EnergyTest.cs
@@ -2,12 +2,12 @@ using Xunit;
 public class EnergyTest
 {
     [Fact]
-    public void EnergyRestoreWhileWalking()
+    public void ConsumeEnergyTest()
     {
         Map map = new Map(5, null, false);
         Player player = Player._instance = new Player("TestPlayer");
         player.CurAction.Set(Creature.sword, Creature.sword.skills[0]);
-        Assert.True(player.CurAction.CurrentBehav is not null);
+
         Program.OnTurn?.Invoke();
         Assert.NotEqual(player.Energy.Max, player.Energy.Cur);
     }
