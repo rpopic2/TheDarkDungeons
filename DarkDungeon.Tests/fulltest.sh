96de0f88dac9b02f070a19e6575785438b018c9e
chaeri <noreply@google.com>
2022-05-22 15:50:27 +0900

    added fulltest

---
+++ DarkDungeon.Tests/fulltest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# auto dotnet test runner by rpopic2
+# last edit 22 May 2022
+echo Starting tests...
+dotnet build -v=q --nologo
+rm testlist 2> /dev/null
+list=$(find .. -name \*Test.cs)
+for testitem in $list; do
+    tempname=$(basename $testitem .cs)
+    echo $tempname >> testlist
+    echo Found test file $tempname
+done
+
+for testitem2 in `cat testlist`; do
+    echo -------------- $testitem2 -------------- 
+    dotnet test --nologo --verbosity=q --no-build --filter=$testitem2
+done
