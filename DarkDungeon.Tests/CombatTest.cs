d92f6bee8a1e78dfce9c24bc42dd3a3cac447289
chaeri <noreply@google.com>
2022-05-22 14:15:29 +0900

    removed combattest

--- DarkDungeon.Tests/CombatTest.cs
+++
@@ -1,8 +0,0 @@
-//using Xunit;
-public class CombatTest
-{
-    private Player player = Player._instance = new("TestPlayer");
-    private Map map = new(5, null, false);
-    private Monster monster = new Bat(new(1, Facing.Left));
-}
-
--- DarkDungeon/Map.cs
+++ DarkDungeon/Map.cs
@@ -25,6 +25,7 @@ public class Map
     private Action _onTurnEnd = () => { };
     public Map(int length, Corpse? corpseFromPrev, bool spawnMobs = true)
     {
+        Console.WriteLine("New Map");
         Current = this;
         Program.OnTurn = () => OnTurnElapse();
         this.Length = length;
