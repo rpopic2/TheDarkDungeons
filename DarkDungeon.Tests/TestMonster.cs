b412739bf81f66fcb6d760c3aa17d8440fa3030f
chaeri <noreply@google.com>
2022-05-23 10:35:46 +0900

    moved testmon to test folder

--- DarkDungeon.Tests/TestMonster.cs
+++ DarkDungeon.Tests/TestMonster.cs
@@ -1,11 +1,8 @@
 public class TestMonster : Monster, ISpawnable
 {
     public static StatInfo TestMonStat = new(new(1, 1, 1), 3, 1);
-    public static MonsterData TestMonData = new("TestMon", 't', 'T', TestMonStat, new Item[] {});
-    public TestMonster(Position spawnPoint, MonsterData? data = null) : base(TestMonData, spawnPoint)
-    {
-
-    }
+    public static MonsterData TestMonData = new("TestMon", 't', 'T', TestMonStat, new Item[] { });
+    public TestMonster(Position spawnPoint, MonsterData? data = null) : base(TestMonData, spawnPoint) { }
 
     public Monster Instantiate(Position spawnPoint) => new TestMonster(spawnPoint, TestMonData);
 
@@ -18,10 +15,7 @@ public class TestMonster : Monster, ISpawnable
     {
         throw new NotImplementedException();
     }
-    protected override void OnNothing()
-    {
-
-    }
+    protected override void OnNothing() { }
 
     public void GiveItem(Item item)
     {
