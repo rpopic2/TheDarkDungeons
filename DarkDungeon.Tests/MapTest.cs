8324ee446eee4e2d063de5ad2c5da8d2b79c7bf0
chaeri <noreply@google.com>
2022-05-23 14:31:15 +0900

    maptest uses disposable pattern

--- DarkDungeon.Tests/MapTest.cs
+++ DarkDungeon.Tests/MapTest.cs
@@ -1,18 +1,26 @@
 using System;
 using Xunit;
-public class MapTest
+public class MapTest : IDisposable
 {
+    Map? _map;
+    Map map => _map!;
+    public MapTest()
+    {
+        _map = new Map(5, false, true);
+    }
+    public void Dispose()
+    {
+        _map = null;
+    }
     [Fact]
     public void CreateMapWithPortal()
     {
-        Map map = new Map(5, false, true);     
         ISteppable?[] steppables = map.Steppables;
         Assert.NotEqual(-1, Array.LastIndexOf<ISteppable?>(steppables, new Portal()));
     }
     [Fact]
     public void CreatePortallessMap()
     {
-        Map map = new Map(5, false, false);     
         ISteppable?[] steppables = map.Steppables;
         int portalIndex = Array.LastIndexOf<ISteppable?>(steppables, new Portal());
         Assert.Equal(-1, portalIndex);
@@ -20,11 +28,15 @@ public class MapTest
     [Fact]
     public void NonInteractiveTurnElapse()
     {
-        Map map = new Map(5, true);
         Player player = Player._instance = new Player("TestPlayer");
         Assert.Equal(0, Map.Turn);
 
         Program.OnTurn?.Invoke();
         Assert.Equal(1, Map.Turn);
+    }
+    [Fact]
+    public void GetElementAtTest()
+    {
+
     }
 }
