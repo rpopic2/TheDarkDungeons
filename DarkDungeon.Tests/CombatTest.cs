136bcd75dfebd833cc21b181804544f8820e967e
chaeri <noreply@google.com>
2022-05-23 10:27:11 +0900

    added testmon and its tests for adding item, using items

--- DarkDungeon.Tests/CombatTest.cs
+++ DarkDungeon.Tests/CombatTest.cs
@@ -48,6 +48,39 @@ public class CombatTest : IDisposable
         player.PickupItem(item, metaData2);
 
         Assert.Equal(1, metaData2.CurExp);
+    }
+    [Fact]
+    public void SetDamageByCurActionSet()
+    {
+        player!.PickupItem(Creature.sword);
+        player.CurAction.Set(Creature.sword, Creature.sword.skills[0], 1);
+        Lunatic lunatic = new(new(1, Facing.Right));
+        lunatic.CurAction.Set(Creature.holySword, Creature.holySword.skills[0], 3);
+        Program.OnTurn?.Invoke();
 
+        Assert.Equal(1, lunatic.GetHp().Cur);
+    }
+    [Fact]
+    public void TestMonsterGiveItem()
+    {
+        TestMonster testMonster = new(new(1, Facing.Right));
+        testMonster.GiveItem(Creature.sword);
+        Assert.Equal(Creature.sword, testMonster.Inven[0]);
+    }
+    [Fact]
+    public void TestMonSetAction()
+    {
+        TestMonster testMon = new(new(1, Facing.Right));
+        testMon.GiveItem(Creature.sword);
+        testMon.SetAction(Creature.sword, 0);
+        Assert.Equal(Creature.sword.skills[0], testMon.CurAction.CurrentBehav);
+    }
+    [Fact]
+    public void TestMonPreformSetAction()
+    {
+        TestMonster testMon = new(new(1, Facing.Right));
+        testMon.GiveItem(Creature.sword);
+        testMon.SetAction(Creature.sword, 0);
+        Program.OnTurn?.Invoke();
     }
 }
---
+++ DarkDungeon/TestMonster.cs
@@ -0,0 +1,31 @@
+public class TestMonster : Monster, ISpawnable
+{
+    public static StatInfo TestMonStat = new(new(1, 1, 1), 3, 1);
+    public static MonsterData TestMonData = new("TestMon", 't', 'T', TestMonStat, new Item[] {});
+    public TestMonster(Position spawnPoint, MonsterData? data = null) : base(TestMonData, spawnPoint)
+    {
+
+    }
+
+    public Monster Instantiate(Position spawnPoint) => new TestMonster(spawnPoint, TestMonData);
+
+    protected override void OnEnergyDeplete()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void OnTarget()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GiveItem(Item item)
+    {
+        Inven.Add(item);
+    }
+
+    public void SetAction(Item item, int skillIndex)
+    {
+        CurAction.Set(item, item.skills[skillIndex]);
+    }
+}
