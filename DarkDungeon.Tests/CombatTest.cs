00218aaf2de97657cd4d8164d8b2c20814e433d3
chaeri <noreply@google.com>
2022-05-23 15:40:51 +0900

    ignores coveragereport

--- DarkDungeon.Tests/CombatTest.cs
+++ DarkDungeon.Tests/CombatTest.cs
@@ -3,27 +3,28 @@ using Xunit;
 public class CombatTest : IDisposable
 {
     Map? map;
-    Player? player;
+    Player? _player;
+    Player player => _player!;
     public CombatTest()
     {
         map = new(3, false);
-        player = Player._instance = new Player("test");
+        _player = Player._instance = new Player("test");
     }
     public void Dispose()
     {
         map = null;
-        player = Player._instance = null;
+        _player = Player._instance = null;
         Program.OnTurn = null;
     }
 
     [Fact]
     public void GainExpByHit()
     {
-        player!.PickupItem(Creature.dagger);
+        player.PickupItem(Creature.dagger);
         TestMonster testMon = new(new(1, Facing.Right));
-        player!.CurAction.Set(player.Inven[0]!, Creature.dagger.skills[0], 3);
+        player.CurAction.Set(player.Inven[0]!, Creature.dagger.skills[0], 3);
         Program.OnTurn?.Invoke();
-        Assert.NotEqual(0, player.Inven.GetMeta(Creature.dagger).CurExp);
+        Assert.NotEqual(0, player.Inven.GetMeta(Creature.dagger)!.CurExp);
     }
     [Fact]
     public void PreserveThrowableWeaponExps()
@@ -41,13 +42,13 @@ public class CombatTest : IDisposable
         Assert.NotNull(player.UnderFoot);
 
         Corpse corpse = (Corpse)player.UnderFoot!;
-        ItemMetaData metaData = corpse.droplist.GetMeta(Creature.dagger);
+        ItemMetaData metaData = corpse.droplist.GetMeta(Creature.dagger)!;
         Assert.Equal(1, metaData.CurExp);
 
         corpse.GetItemAndMeta(2, out Item? item, out ItemMetaData? metaData2);
-        player.PickupItem(item, metaData2);
+        player.PickupItem(item!, metaData2!);
 
-        Assert.Equal(1, metaData2.CurExp);
+        Assert.Equal(1, metaData2!.CurExp);
     }
     [Theory]
     [InlineData(0)]
