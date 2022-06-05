d4355eb8a6e23128b95a76e1054c4791af3b803f
chaeri <noreply@google.com>
2022-06-05 23:45:22 +0900

    nm creature._item->_items

--- DarkDungeon/CreatureInven.cs
+++ DarkDungeon/CreatureInven.cs
@@ -1,33 +1,33 @@
 public partial class Creature
 {
-    private List<IItem> _item = new();
+    private List<IItem> _items = new();
     public bool HasItem<T>() where T : IItem
     {
-        return _item.OfType<T>().Any();
+        return _items.OfType<T>().Any();
     }
     public int GetStackOfItem<T>() where T : IItem
     {
-        return _item.OfType<T>().Count();
+        return _items.OfType<T>().Count();
     }
     public T? GetItemOrDefault<T>() where T : IItem
     {
-        return _item.OfType<T>().FirstOrDefault();
+        return _items.OfType<T>().FirstOrDefault();
     }
     public T GetItem<T>() where T : IItem
     {
-        return _item.OfType<T>().FirstOrDefault()!;
+        return _items.OfType<T>().FirstOrDefault()!;
     }
     public void GiveItem(ItemNew item, int stack = 1)
     {
         for (int i = 0; i < stack; i++)
         {
             item.owner = this;
-            _item.Add(item);
+            _items.Add(item);
         }
     }
     public void RemoveItem<T>() where T : IItem
     {
         IItem? targetItem = GetItem<T>();
-        if (targetItem is not null) _item.Remove(targetItem);
+        if (targetItem is not null) _items.Remove(targetItem);
     }
 }
