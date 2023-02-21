eff99daac9e0c2e55df92b12326e553a0dbc1c0e
rpopic2 <rpopic2@gmail.com>
2023-02-21 23:31:41 +0900

    now can safely send message over tcp

--- DarkDungeon/GameSocket.cs
+++ DarkDungeon/GameSocket.cs
@@ -5,6 +5,8 @@ using System.Text;
 public class GameSocket : IIO
 {
     private const string EOM = "<EOM>";
+    private const string RK = "<rk>";
+    private const string CLR = "<clr>";
     private Socket? s_client;
     ~GameSocket() 
     {
@@ -14,20 +16,25 @@ public class GameSocket : IIO
     public async Task New()
     {
         Console.WriteLine("Creating connection...");
+
         using Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         var ipEndPoint = new IPEndPoint(IPAddress.Any, 8080);
         socket.Bind(ipEndPoint);
         socket.Listen();
+
         s_client = await socket.AcceptAsync();
         s_client.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
+
         Console.WriteLine("Connection created!");
     }
 
     public void pr(string value, bool newline = true)
     {
+        if (value == string.Empty) newline = true;
         if (newline) value += "\n";
-        if (value != "<EOT>") value += EOM;
-        Console.WriteLine($"Sending: {value}");
+        var msglen = Encoding.UTF8.GetByteCount(value);
+        Console.WriteLine($"Sending: {value.Length}: {value}");
+        s_client?.Send(BitConverter.GetBytes(msglen));
         s_client?.Send(Encoding.UTF8.GetBytes(value));
     }
     public void eot()
@@ -37,9 +44,10 @@ public class GameSocket : IIO
 
     public ConsoleKeyInfo rk()
     {
+        pr(RK, false);
         var buffer = new byte[1];
         Console.WriteLine("Waiting for input...");
-        var bytes_read = s_client?.Receive(buffer);
+        var bytes_read = s_client?.Receive(buffer, 1, SocketFlags.None);
         if (bytes_read == 0)
         {
             Console.WriteLine("Connection closed by client.");
@@ -55,6 +63,6 @@ public class GameSocket : IIO
     public void clr()
     {
         Console.WriteLine("Clearing screen...");
-        s_client?.Send(Encoding.UTF8.GetBytes(@"\x1b[2J"));
+        pr(CLR, false);
     }
 }
